e73904ea76fca1680298598dcb59893269581494
Solovev <solovtev17@bk.ru>
2025-06-19 17:13:00 +0300

    added migrations

---
+++ backend/src/Accounts/PetFamily.Accounts.Infrastructure/Migrations/20250619135040_Initial_Account_module.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PetFamily.Accounts.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Account_module : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Accounts");
+
+            migrationBuilder.CreateTable(
+                name: "permission",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permission", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    NormalizedName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PermissionRole",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    PermissionsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RolesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionRole", x => new { x.PermissionsId, x.RolesId });
+                    table.ForeignKey(
+                        name: "FK_PermissionRole_permission_PermissionsId",
+                        column: x => x.PermissionsId,
+                        principalSchema: "Accounts",
+                        principalTable: "permission",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PermissionRole_roles_RolesId",
+                        column: x => x.RolesId,
+                        principalSchema: "Accounts",
+                        principalTable: "roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "role_claims",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role_claims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_role_claims_roles_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "Accounts",
+                        principalTable: "roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    social_networks = table.Column<string>(type: "jsonb", nullable: false),
+                    email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    role_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    username = table.Column<string>(type: "text", nullable: true),
+                    full_name = table.Column<string>(type: "text", nullable: false),
+                    UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "text", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_users_roles_role_id",
+                        column: x => x.role_id,
+                        principalSchema: "Accounts",
+                        principalTable: "roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "admins_accounts",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_admins_accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_admins_accounts_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "Accounts",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "participants_accounts",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    favorite_pets_ids = table.Column<List<Guid>>(type: "uuid[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_participants_accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_participants_accounts_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "Accounts",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_claims",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_claims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_user_claims_users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Accounts",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_logins",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "text", nullable: false),
+                    ProviderKey = table.Column<string>(type: "text", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_logins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_user_logins_users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Accounts",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_tokens",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LoginProvider = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_tokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_user_tokens_users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Accounts",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "volunteers_accounts",
+                schema: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    certificates = table.Column<string>(type: "text", nullable: true),
+                    details_for_help = table.Column<string>(type: "jsonb", nullable: false),
+                    experience = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_volunteers_accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_volunteers_accounts_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "Accounts",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_admins_accounts_user_id",
+                schema: "Accounts",
+                table: "admins_accounts",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_participants_accounts_user_id",
+                schema: "Accounts",
+                table: "participants_accounts",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PermissionRole_RolesId",
+                schema: "Accounts",
+                table: "PermissionRole",
+                column: "RolesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_role_claims_RoleId",
+                schema: "Accounts",
+                table: "role_claims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                schema: "Accounts",
+                table: "roles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_claims_UserId",
+                schema: "Accounts",
+                table: "user_claims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_logins_UserId",
+                schema: "Accounts",
+                table: "user_logins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                schema: "Accounts",
+                table: "users",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_role_id",
+                schema: "Accounts",
+                table: "users",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                schema: "Accounts",
+                table: "users",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_volunteers_accounts_user_id",
+                schema: "Accounts",
+                table: "volunteers_accounts",
+                column: "user_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "admins_accounts",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "participants_accounts",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "PermissionRole",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "role_claims",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "user_claims",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "user_logins",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "user_tokens",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "volunteers_accounts",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "permission",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "roles",
+                schema: "Accounts");
+        }
+    }
+}
---
+++ backend/src/Volunteers/PetFamily.Volunteers.Infrastructure/Migrations/20250618132013_initial_volunteers_module.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.Volunteers.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_volunteers_module : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Volunteers");
+
+            migrationBuilder.CreateTable(
+                name: "volunteers",
+                schema: "Volunteers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    files = table.Column<string>(type: "jsonb", nullable: false),
+                    first_name = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    surname = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    number = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false),
+                    region_code = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_volunteers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pets",
+                schema: "Volunteers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    serial_number = table.Column<long>(type: "bigint", nullable: false),
+                    age = table.Column<long>(type: "bigint", nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    color = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    weight = table.Column<double>(type: "double precision", nullable: false),
+                    height = table.Column<double>(type: "double precision", nullable: false),
+                    sterilize = table.Column<bool>(type: "boolean", nullable: false),
+                    date_of_birth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    vaccinated = table.Column<bool>(type: "boolean", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    details_for_help = table.Column<string>(type: "jsonb", nullable: false),
+                    files = table.Column<string>(type: "jsonb", nullable: false),
+                    volunteer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    apartment_number = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: true),
+                    city = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    country = table.Column<string>(type: "character varying(75)", maxLength: 75, nullable: false),
+                    house_number = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false),
+                    street = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    digestive_system_condition = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    mouth_condition = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    shared_health_status = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    skin_condition = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    number = table.Column<string>(type: "text", nullable: false),
+                    region_code = table.Column<string>(type: "text", nullable: false),
+                    breed_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    species_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pets", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_pets_volunteers_volunteer_id",
+                        column: x => x.volunteer_id,
+                        principalSchema: "Volunteers",
+                        principalTable: "volunteers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pets_volunteer_id",
+                schema: "Volunteers",
+                table: "pets",
+                column: "volunteer_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pets",
+                schema: "Volunteers");
+
+            migrationBuilder.DropTable(
+                name: "volunteers",
+                schema: "Volunteers");
+        }
+    }
+}
